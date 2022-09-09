38c699fba187e09d85e4b63f9aca3857fb59626e
Giuseppe Capizzi <g.capizzi@gmail.com>
2022-09-09 02:45:51 +0200

    Remove the oauth2 dependency
    This replaces oauth2 with my own implementation of the authentication
    flow, using http_client::HttpClient.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ mod twitter;
 use futures::executor::block_on;
 
 fn main() -> Result<(), anyhow::Error> {
-    let token = oauth::get_token()?;
-    let http_client = http::AuthenticatedClient::new(http_client::h1::H1Client::new(), token);
-    let client = twitter::Client::new(http_client);
+    let token = block_on(oauth::get_token(http_client::h1::H1Client::new()))?;
+    let authenticated_client =
+        http::AuthenticatedClient::new(http_client::h1::H1Client::new(), token);
+    let client = twitter::Client::new(authenticated_client);
 
     dbg!(block_on(client.tweets([
         "1460323737035677698",
--- src/oauth.rs
+++ src/oauth.rs
@@ -1,9 +1,11 @@
 use anyhow::anyhow;
-use oauth2::TokenResponse;
 use serde::{Deserialize, Serialize};
 
 const AUTH_URL: &str = "https://twitter.com/i/oauth2/authorize";
 const TOKEN_URL: &str = "https://api.twitter.com/2/oauth2/token";
+
+const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const PASSWORD_LEN: usize = 128;
 
 #[derive(Deserialize, Serialize)]
 struct Token {
@@ -12,10 +14,10 @@ struct Token {
 }
 
 impl Token {
-    fn new(access_token: String, expires_in: std::time::Duration) -> Token {
+    fn new(access_token: String, expires_in: u64) -> Token {
         Token {
             access_token,
-            expires_at: std::time::SystemTime::now() + expires_in,
+            expires_at: std::time::SystemTime::now() + std::time::Duration::from_secs(expires_in),
         }
     }
 
@@ -28,8 +30,12 @@ impl Token {
     }
 }
 
-pub fn get_token() -> Result<String, anyhow::Error> {
-    let token = load_from_disk().or_else(|_| login().and_then(save_to_disk))?;
+pub async fn get_token(http_client: impl http_client::HttpClient) -> Result<String, anyhow::Error> {
+    let token = if let Ok(t) = load_from_disk() {
+        t
+    } else {
+        login(http_client).await.and_then(save_to_disk)?
+    };
     Ok(token.access_token().clone())
 }
 
@@ -50,46 +56,68 @@ fn save_to_disk(token: Token) -> Result<Token, anyhow::Error> {
     Ok(token)
 }
 
-fn login() -> Result<Token, anyhow::Error> {
+#[derive(Debug, Serialize, Deserialize)]
+struct TokenRequestBody {
+    code: String,
+    grant_type: String,
+    redirect_uri: String,
+    code_verifier: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TokenResposeBody {
+    token_type: String,
+    expires_in: u64,
+    access_token: String,
+    scope: String,
+}
+
+async fn login(http_client: impl http_client::HttpClient) -> Result<Token, anyhow::Error> {
     let redirect_addr = "0.0.0.0:8000";
-    let redirect_url = oauth2::RedirectUrl::new(format!("http://{}", redirect_addr))?;
+    let redirect_uri = format!("http://{}", redirect_addr);
 
-    let client_id = oauth2::ClientId::new(std::env::var("TWT_CLIENT_ID")?);
-    let client_secret = oauth2::ClientSecret::new(std::env::var("TWT_CLIENT_SECRET")?);
-    let auth_url = oauth2::AuthUrl::new(AUTH_URL.to_string())?;
-    let token_url = oauth2::TokenUrl::new(TOKEN_URL.to_string())?;
-    let oauth2_client =
-        oauth2::basic::BasicClient::new(client_id, Some(client_secret), auth_url, Some(token_url))
-            .set_redirect_uri(redirect_url);
+    let client_id = std::env::var("TWT_CLIENT_ID")?;
+    let client_secret = std::env::var("TWT_CLIENT_SECRET")?;
 
-    let (pkce_challenge, pkce_verifier) = oauth2::PkceCodeChallenge::new_random_sha256();
+    let pkce_verifier = random_chars(PASSWORD_LEN);
+    let pkce_challenge = base64::encode_config(sha256(&pkce_verifier), base64::URL_SAFE_NO_PAD);
+    let state = random_string(PASSWORD_LEN)?;
 
-    let (auth_url, csrf_state) = oauth2_client
-        .authorize_url(oauth2::CsrfToken::new_random)
-        .add_scope(oauth2::Scope::new("tweet.read".to_string()))
-        .add_scope(oauth2::Scope::new("users.read".to_string()))
-        .set_pkce_challenge(pkce_challenge)
-        .url();
+    let mut auth_url = url::Url::parse(AUTH_URL)?;
+    auth_url
+        .query_pairs_mut()
+        .append_pair("response_type", "code")
+        .append_pair("client_id", client_id.as_str())
+        .append_pair("redirect_uri", redirect_uri.as_str())
+        .append_pair("scope", "tweet.read users.read")
+        .append_pair("state", state.as_str())
+        .append_pair("code_challenge", pkce_challenge.as_str())
+        .append_pair("code_challenge_method", "S256");
 
     println!("Browse to: {}", auth_url);
 
-    let (code, state) = receive_redirect(redirect_addr)?;
+    let (code, received_state) = receive_redirect(redirect_addr)?;
 
-    if &state != csrf_state.secret() {
+    if received_state != state {
         return Err(anyhow!("wrong state!"));
     }
 
-    let token_response = oauth2_client
-        .exchange_code(oauth2::AuthorizationCode::new(code))
-        .set_pkce_verifier(pkce_verifier)
-        .request(oauth2::ureq::http_client)?;
+    let mut token_req = http_types::Request::new(http_types::Method::Post, TOKEN_URL);
+    let req_body = TokenRequestBody {
+        code,
+        grant_type: "authorization_code".to_string(),
+        redirect_uri,
+        code_verifier: String::from_utf8(pkce_verifier)?,
+    };
+    let body = http_types::Body::from_form(&req_body).map_err(|e| anyhow!(e))?;
+    token_req.set_body(body);
+    let basic_auth = base64::encode(format!("{}:{}", client_id, client_secret));
+    token_req.insert_header("Authorization", format!("Basic {}", basic_auth));
 
-    Ok(Token::new(
-        token_response.access_token().secret().clone(),
-        token_response
-            .expires_in()
-            .ok_or(anyhow!("no expires_in field"))?,
-    ))
+    let mut token_resp = http_client.send(token_req).await.map_err(|e| anyhow!(e))?;
+    let resp_body: TokenResposeBody = token_resp.body_json().await.map_err(|e| anyhow!(e))?;
+
+    Ok(Token::new(resp_body.access_token, resp_body.expires_in))
 }
 
 fn receive_redirect(addr: &str) -> Result<(String, String), anyhow::Error> {
@@ -109,4 +137,26 @@ fn receive_redirect(addr: &str) -> Result<(String, String), anyhow::Error> {
         .1;
     request.respond(tiny_http::Response::from_string("done!"))?;
     Ok((String::from(code), String::from(state)))
+}
+
+fn random_chars(length: usize) -> Vec<u8> {
+    use rand::Rng;
+    (0..length)
+        .map(|_| {
+            let idx = rand::thread_rng().gen_range(0..CHARSET.len());
+            CHARSET[idx]
+        })
+        .collect()
+}
+
+fn random_string(length: usize) -> Result<String, anyhow::Error> {
+    Ok(String::from_utf8(random_chars(length))?)
+}
+
+fn sha256(data: impl AsRef<[u8]>) -> impl AsRef<[u8]> {
+    use sha2::{Digest, Sha256};
+
+    let mut hasher = Sha256::new();
+    hasher.update(&data);
+    hasher.finalize()
 }
