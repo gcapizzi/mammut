9df2a64e3da359f730da40a7e1d0b473312f0a0a
Giuseppe Capizzi <g.capizzi@gmail.com>
2022-10-22 21:18:52 +0200

    Test token refreshing failure

--- src/oauth/tests.rs
+++ src/oauth/tests.rs
@@ -157,6 +157,84 @@ async fn when_the_token_is_cached_but_expired_and_refreshable_it_refreshes_it_an
     expect(&cached_token.is_expired()).to(equal(false));
 }
 
+#[async_std::test]
+async fn when_refreshing_fails_it_logins_again_and_saves_the_token() {
+    let authenticator = mock::Authenticator::new("the-auth-code".to_string());
+    let http_client = mock::HttpClient::new([
+        mock::HttpResponse {
+            status: 500,
+            body: "".to_string(),
+        },
+        mock::HttpResponse {
+            status: 200,
+            body: "{
+            \"token_type\": \"bearer\",
+            \"expires_in\": 1,
+            \"access_token\": \"NEW_ACCESS_TOKEN\",
+            \"refresh_token\": \"NEW_REFRESH_TOKEN\",
+            \"scope\": \"SCOPE\"
+        }"
+            .to_string(),
+        },
+    ]);
+    let token = oauth::Token::new(
+        "CACHED_ACCESS_TOKEN".to_string(),
+        Some("CACHED_REFRESH_TOKEN".to_string()),
+        0,
+    );
+    let cache = mock::TokenCache::with_value(token.clone());
+
+    let client = oauth::Client::new(
+        &http_client,
+        &authenticator,
+        &cache,
+        oauth::Config {
+            client_id: "id",
+            client_secret: "secret",
+            auth_url: "https://the-auth-url",
+            token_url: "https://the-token-url",
+            redirect_url: "https://the-redirect-url",
+        },
+    );
+
+    expect(&client.get_access_token().await.unwrap()).to(equal("NEW_ACCESS_TOKEN".to_string()));
+
+    let auth_url = authenticator.last_auth_url().unwrap();
+    expect(&auth_url.origin().unicode_serialization()).to(equal("https://the-auth-url"));
+
+    let auth_url_params: HashMap<_, _> = auth_url.query_pairs().into_owned().collect();
+    expect(auth_url_params.get("response_type").unwrap()).to(equal("code"));
+    expect(auth_url_params.get("client_id").unwrap()).to(equal("id"));
+    expect(auth_url_params.get("scope").unwrap()).to(equal("tweet.read users.read offline.access"));
+    expect(&auth_url_params.get("code_challenge_method")).to(equal(Some(&"S256".to_string())));
+
+    let redirect_url = auth_url_params.get("redirect_uri").unwrap();
+    expect(&redirect_url).to(equal("https://the-redirect-url"));
+
+    let code_challenge = auth_url_params.get("code_challenge").unwrap();
+
+    let reqs = http_client.requests();
+    let token_req = reqs.last().unwrap().clone();
+
+    expect(&token_req.method).to(equal("POST"));
+    expect(&token_req.url).to(equal("https://the-token-url"));
+
+    // base64("id:secret") = "aWQ6c2VjcmV0"
+    expect(token_req.headers.get("authorization").unwrap()).to(equal("Basic aWQ6c2VjcmV0"));
+
+    expect(&token_req.body.get("code").unwrap()).to(equal("the-auth-code"));
+    expect(&token_req.body.get("grant_type").unwrap()).to(equal("authorization_code"));
+    expect(&token_req.body.get("redirect_uri").unwrap()).to(equal(redirect_url));
+
+    let code_verifier = token_req.body.get("code_verifier").unwrap();
+    expect(&code_challenge).to(equal(&sha256(code_verifier)));
+
+    let cached_token = cache.get().unwrap();
+    expect(&cached_token.access_token()).to(equal(&"NEW_ACCESS_TOKEN".to_string()));
+    expect(&cached_token.refresh_token()).to(equal(&Some("NEW_REFRESH_TOKEN".to_string())));
+    expect(&cached_token.is_expired()).to(equal(false));
+}
+
 fn sha256(data: &str) -> String {
     use sha2::{Digest, Sha256};
     let mut hasher = Sha256::new();
